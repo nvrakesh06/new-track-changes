bbef72a64faf9cbaa9795b2d5787996066bfcfbf
nvrakesh06 <hsekarwtf@gmail.com>
2024-03-10 19:52:24 +0530

    Initial commit

---
+++ file1.py
@@ -0,0 +1,4 @@
+def sum_of_n_numbers(n):
+    return (n * (n + 1)) // 2 
+
+print(sum_of_n_numbers(1000))
---
+++ file2.py
@@ -0,0 +1,5 @@
+def sum_of_even_numbers_till_n(n):
+    n = n // 2
+    return (n * (n + 1))
+
+print(sum_of_even_numbers_till_n(1000))
